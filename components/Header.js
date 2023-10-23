c1380ef20a72bdd0d60d07205c7f7774b38dd3a3
jamesonarcher <jamesonmarcher@gmail.com>
2023-10-23 02:53:21 -0400

    fixed navbar responsiveness

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
           {/* logo */}
           <Link href={'/'}>
             <Image
-              src={'/favicon.svg'}
+              src={'/logo.svg'}
               width={220}
               height={48}
               alt=''
